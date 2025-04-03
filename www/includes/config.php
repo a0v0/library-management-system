698075cc4da6b84009c7395a8b84148c6aec716e
aV <avm244@proton.me>
2025-04-03 14:11:34 +0530

    sad

--- www/includes/config.php
+++ www/includes/config.php
@@ -5,6 +5,7 @@ define('DB_USER', 'root');
 define('DB_PASS', 'root');
 define('DB_NAME', 'library');
 // Establish database connection.
+
 try {
     $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 } catch (PDOException $e) {
